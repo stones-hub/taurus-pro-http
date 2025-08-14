5b005c43e564f24cbba811e52ffc320818cba805
STones_ <61647649@qq.com>
2025-08-14 11:41:52 +0800

    fixed: cors 校验

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -246,7 +246,9 @@ func CorsMiddleware(config *CorsConfig) func(http.Handler) http.Handler {
 				// 非预检请求只检查自定义头（非标准头）
 				var customHeaders []string
 				for headerName := range r.Header {
+					log.Printf("[CORS] 请求头: %s", headerName)
 					if !isStandardHeader(strings.ToLower(headerName)) {
+						log.Printf("[CORS] 自定义请求头: %s", headerName)
 						customHeaders = append(customHeaders, headerName)
 					}
 				}
